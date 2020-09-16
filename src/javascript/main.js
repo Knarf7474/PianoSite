0a039274b48e6f361fb2c78a9b36bab411240b3a
Frank van Beem <fpvanbeem@gmail.com>
2020-09-16 20:57:52 +0200

    tweaks + self playing piano

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,5 @@
 let code = [];
+const correct_code = 'GCCCGDBC';
 
 const piano = SampleLibrary.load({
   instruments: "piano"
@@ -18,14 +19,19 @@ document.querySelectorAll('.key').forEach(function(key_elem){
 
       if (code.length === 8){
         //Check if code correct
-        if (code.join('') === 'GCCCGDBC'){ // HEEEEY!! NIET VALSSPELEN JIJ!
-          setTimeout(function() {
-            document.querySelector('.piano-part').style.display = 'none';
-            document.querySelector('.reveal-part').style.display = 'block';
-            const audio = document.querySelector('.wedding_music')
-            audio.loop = true
-            audio.play();
-          }, 500 )
+        if (code.join('') === correct_code){ // HEEEEY!! NIET VALSSPELEN JIJ!
+          setTimeout(async function() {
+            await sleep(1000);
+            await play_song();
+
+            setTimeout(function () {
+              document.querySelector('.piano-part').style.display = 'none';
+              document.querySelector('.reveal-part').style.display = 'block';
+              const audio = document.querySelector('.wedding_music')
+              audio.loop = true
+              audio.play();
+            }, 500)
+          })
         } else {
           document.querySelector('.invalid_message').style.display = 'block'
           // setTimeout(function() {
@@ -58,13 +64,63 @@ document.querySelector('.stop_music').addEventListener('click', async function(e
   audio.pause();
 })
 
-
-
 function update_code_visual(){
+  let incorrect_puzzles = []
   const code_section = document.querySelector('.code')
   const fields = Array.from(document.querySelectorAll('.code span .text'))
   for(let i = 0; i < 8; i++){
-    fields[i].innerText = code[i] || '';
+    const content = code[i] || '';
+    fields[i].innerText = content
+    if (content){
+      fields[i].parentNode.classList.add('filled')
+      if (code[i] !== correct_code[i]) {
+        const puzzle_num = parseInt(i / 2) + 1
+        if(incorrect_puzzles.indexOf(puzzle_num) === -1) {
+          incorrect_puzzles.push(puzzle_num)
+        }
+      }
+    } else {
+      fields[i].parentNode.classList.remove('filled')
+    }
+  }
+
+  if (incorrect_puzzles.length === 1) {
+    document.querySelector('.invalid_message .puzzles').innerText = 'puzzel ' + incorrect_puzzles[0]
+  } else if(incorrect_puzzles.length > 1) {
+    document.querySelector('.invalid_message .puzzles').innerText = 'puzzels ' + incorrect_puzzles.join(' & ')
   }
 }
 
+async function play_song(one,two,three,four){
+  const speed_factor = 1.4
+  await strike_key('G')
+  await sleep(400 * speed_factor);
+  await strike_key('C')
+  await sleep(300 * speed_factor);
+  await strike_key('C')
+  await sleep(200 * speed_factor);
+  await strike_key('C')
+  await sleep(700 * speed_factor);
+
+  await strike_key('G')
+  await sleep(400 * speed_factor);
+  await strike_key('D')
+  await sleep(300 * speed_factor);
+  await strike_key('B')
+  await sleep(200 * speed_factor);
+  await strike_key('C')
+  await sleep(700 * speed_factor);
+}
+
+async function strike_key(note, time){
+  const key = document.querySelector('.key.white.'+ note)
+  key.click()
+  key.classList.add('active')
+  setTimeout(function(){
+      key.classList.remove('active')
+  }, 200)
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
